faf78552b72a70397c23dfe04e9ea92ac518d865
Lautaro Lamaita <lautarolamaita@gmail.com>
2026-09-02 00:37:54 -0300

    feat(dao-negocio): UnidadDeVenta con consulta traerUnidadYPlatos

---
+++ src/dao/UnidadDeVentaDao.java
@@ -0,0 +1,119 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import datos.UnidadDeVenta;
+
+public class UnidadDeVentaDao {
+
+	private static Session session;
+	private Transaction tx;
+
+	private static UnidadDeVentaDao dao = null;
+
+	protected UnidadDeVentaDao() {}
+
+	public static UnidadDeVentaDao getInstancia() {
+		if (dao == null) {
+			dao = new UnidadDeVentaDao();
+		}
+		return dao;
+	}
+
+	private void iniciaOperacion() throws HibernateException {
+		session = HibernateUtil.getSessionFactory().openSession();
+		tx = session.beginTransaction();
+	}
+
+	private void manejaExcepcion(HibernateException he) throws HibernateException {
+		tx.rollback();
+		throw new HibernateException("ERROR en la capa de acceso a datos", he);
+	}
+
+
+	public int agregar(UnidadDeVenta objeto) {
+		int id = 0;
+		try {
+			iniciaOperacion();
+			id = Integer.parseInt(session.save(objeto).toString());
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+		return id;
+	}
+
+	public void actualizar(UnidadDeVenta objeto) {
+		try {
+			iniciaOperacion();
+			session.update(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+	}
+
+	public void eliminar(UnidadDeVenta objeto) {
+		try {
+			iniciaOperacion();
+			session.delete(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+	}
+	
+	//devuelve FoodTruck o PuestoDesarmable segun corresponda
+		public UnidadDeVenta traer(long idUnidad) {
+			UnidadDeVenta objeto = null;
+			try {
+				iniciaOperacion();
+				objeto = (UnidadDeVenta) session.get(UnidadDeVenta.class, idUnidad);
+			} finally {
+				session.close();
+			}
+			return objeto;
+		}
+	
+		public List<UnidadDeVenta> traer() {
+			List<UnidadDeVenta> lista = null;
+			try {
+				iniciaOperacion();
+				lista = session.createQuery("from UnidadDeVenta u order by u.nombreComercial asc",
+						UnidadDeVenta.class).list();
+			} finally {
+				session.close();
+			}
+			return lista;
+		}
+
+		public UnidadDeVenta traerUnidadYPlatos(long idUnidad) throws HibernateException {
+			UnidadDeVenta objeto = null;
+			try {
+				iniciaOperacion();
+				String hql = "select distinct u from UnidadDeVenta u "
+						   + "left join fetch u.platos p "
+						   + "where u.id = :idUnidad "
+						   + "order by p.precioVenta desc";
+				objeto = (UnidadDeVenta) session.createQuery(hql, UnidadDeVenta.class)
+						.setParameter("idUnidad", idUnidad)
+						.uniqueResult();
+			} finally {
+				session.close();
+			}
+			return objeto;
+		}
+}	
---
+++ src/negocio/UnidadDeVentaABM.java
@@ -0,0 +1,46 @@
+package negocio;
+
+import java.util.List;
+
+import dao.UnidadDeVentaDao;
+import datos.UnidadDeVenta;
+
+public class UnidadDeVentaABM {
+
+	private static UnidadDeVentaABM abm = null;
+	private UnidadDeVentaDao dao = UnidadDeVentaDao.getInstancia();
+
+	private UnidadDeVentaABM() {}
+
+	public static UnidadDeVentaABM getInstancia() {
+		if (abm == null) {
+			abm = new UnidadDeVentaABM();
+		}
+		return abm;
+	}
+
+	public int agregar(UnidadDeVenta u) {
+		return dao.agregar(u);
+	}
+
+	public void modificar(UnidadDeVenta u) {
+		dao.actualizar(u);
+	}
+
+	public void eliminar(long idUnidad) {
+		UnidadDeVenta u = dao.traer(idUnidad);
+		dao.eliminar(u);
+	}
+
+	public UnidadDeVenta traer(long idUnidad) {
+		return dao.traer(idUnidad);
+	}
+
+	public List<UnidadDeVenta> traer() {
+		return dao.traer();
+	}
+
+	public UnidadDeVenta traerUnidadYPlatos(long idUnidad) {
+		return dao.traerUnidadYPlatos(idUnidad);
+	}
+}
